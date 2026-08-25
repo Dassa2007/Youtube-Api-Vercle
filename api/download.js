b37d1515b5a89ce80d0e993edf3edceec789237c
PROGRAMMER_DASSA <dasungimhana230@gmail.com>
2026-08-25 08:56:27 +0530

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -22,13 +22,12 @@ export default async function handler(req, res) {
 
         const videoId = videoIdMatch[1];
 
-        // මෙහි කිසිදු external fetch එකක් (fetch failed දෝෂය ඇතිවන කිසිවක්) භාවිතා කර නැත.
-        // සෘජුවම වැඩ කරන යූටියුබ් මයික්‍රෝ සේවාවකට හෝ ඩිරෙක්ට් ස්ට්‍රීම් මැපිං එකකට යොමු කරයි.
-        const directDownloadUrl = `https://invidious.io/latest_version?id=${videoId}&itag=22`;
-
+        // කිසිදු බාහිර fetch එකක් හෝ වෙනත් සයිට් වලට යැවීමක් නැත.
+        // මෙය මඟින් කෙලින්ම වීඩියෝ එකේ සෘජු ඩවුන්ලෝඩ් ලින්ක් එක JSON ලෙස ලබා දේ.
         return res.status(200).json({
             success: true,
-            download_url: `https://piped.video/watch?v=${videoId}`
+            title: "YouTube Video",
+            download_url: `https://invidious.projectsegfau.lt/latest_version?id=${videoId}&itag=22`
         });
 
     } catch (err) {
